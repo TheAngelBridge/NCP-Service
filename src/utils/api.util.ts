a45e1e54e63e331d76c02989ff0c7ffa61a9772f
AlenHeo <alenheo@theangelbridge.net>
2022-05-24 10:53:52 +0900

    feat/main: ADD ApiRequest wrapper

--- src/utils/api.util.ts
+++ src/utils/api.util.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance, AxiosResponse, Method } from "axios";
+import { ApiError, ApiErrorCase } from "../errors/api.error";
 import { ApiRequest } from "../interfaces/api.interface";
 
 
@@ -19,7 +20,43 @@ export class ApiClient {
 
 
 
-
+    /**
+     * Execute request and return Promise with error or response if success
+     * 
+     *  Wrapping axios-request with Promise's resolve / reject for error handling 
+     * 
+     * @template T - Data type matched with each API's response
+     * @access private
+     * @param apiRequest `ApiRequest` configs for each api service
+     * @returns `Promise<T>` return promised response of http request with current ApiRequest configs and handle errors
+     * @memberof ApiClient
+     */
+    private createRequest<T>(apiRequest: ApiRequest): Promise<T> {
+        return new Promise<T>((resolve, reject) => {
+            this.axiosRequest(apiRequest)
+                .then((response: any) => {
+                    resolve(response.data as T)
+                })
+                .catch((error) => {
+                    if (axios.isAxiosError(error)) {
+                        if (error.response) {
+                            reject(new ApiError(ApiErrorCase.httpStatusCode, error.response.status))
+                        } else if (error.request) {
+                            reject(new ApiError(ApiErrorCase.noResponseFromServer))
+                        } else {
+                            reject(new ApiError(ApiErrorCase.requestConfigurationError))
+                        }
+                    } else if (error instanceof ApiError) {
+                        // url validation Error
+                        reject(error)
+                    } else {
+                        // unexpected Error
+                        reject(new ApiError(ApiErrorCase.unexpectedResponse))
+                    }
+                })
+        })
+    }
+    
 
     /**
      *  Return configured AxiosInstance's request execution
@@ -33,7 +70,7 @@ export class ApiClient {
     private axiosRequest(apiRequest: ApiRequest): Promise<AxiosResponse> {
         const { path, method, headers, body } = apiRequest
         // url validation
-        if (!this.validateURL(this.client.defaults.baseURL + apiRequest.path)) console.log('error handling')
+        if (!this.validateURL(this.client.defaults.baseURL + apiRequest.path)) throw new ApiError(ApiErrorCase.invalidURL)
         return this.client.request({
             url: path,
             method: method,
